221af5b8fb85304326e8b06ffaf3ccc0547c18d0
Marianne Wihsbeck <marianne.wihsbeck@a1.net>
2024-11-03 11:09:23 +0100

    First commit

---
+++ tic-tac-toe.py
@@ -0,0 +1,47 @@
+game_board = "--------------------"
+def evaluate (game_status):
+    # "x" – The player who uses crosses (Xs) has won (the board contains xxx)
+    # "o" – The player who uses noughts (Os) has won (the board contains ooo)
+    # "!" – Draw (the board is full but nobody has won)
+    # "-" – Rest (i.e. the game is not finished)
+
+    if "xxx" in game_status:
+        return "x"
+    elif "ooo" in game_status:
+       return "o"
+    elif "-" in game_status:
+        return "-"
+    else:
+        return "!"
+result = evaluate(game_board)
+
+
+def move(board, mark, position):
+    # Returns the game board with the given mark in the given position.
+    first_part_board = board[:position-1]
+    second_part_board = board[position:]
+    new_board = first_part_board+mark+second_part_board
+    return new_board
+
+
+def player_move(game_board):
+    position_input_player = 0
+    while (position_input_player < 1 or position_input_player > 20) or (game_board[position_input_player-1] == "x" or game_board[position_input_player-1] == "o"):
+        position_input_player = int(input("Which position do you want to play [1-20]? ")) # Ask the player which position he wants to play
+        if position_input_player < 1 or position_input_player > 20:
+            print("You need to enter a number between 1 and 20!")
+        elif game_board[position_input_player-1] == "x" or game_board[position_input_player-1] == "o":
+            print("Position already occupied!") # Reject negative or too large numbers or moves to an occupied position
+        else:
+            game_board = move(game_board, "o", position_input_player)
+            return game_board
+print(player_move(game_board))
+
+from random import randrange
+def pc_move(game_board):
+    position_pc = randrange(19)
+    while game_board[position_pc-1] == "x" or game_board[position_pc-1] == "o":
+        position_pc = randrange(19)
+    game_board = move(game_board, "x", position_pc)
+    return game_board
+print(pc_move(game_board))
